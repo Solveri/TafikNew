bee5849ec7aa1dea0e616ac287ebe9911c39cad0
Solveri <100302215+Solveri@users.noreply.github.com>
2025-02-05 20:54:08 +0200

    added probebility
    2

--- Assets/Scripts/Merge/MergeManager.cs
+++ Assets/Scripts/Merge/MergeManager.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI; // Required for UI Images
 public class MergeManager : MonoBehaviour
 {
     [SerializeField] private List<Slot> MergeSlots = new List<Slot>();
-    [SerializeField] private DraggableImage towerPrefab; // Prefab of the Draggable Tower
+    [SerializeField] private List<DraggableImage> draggableImages = new List<DraggableImage>(); // Prefab of the Draggable Tower
 
     public void SpawnTower()
     {
@@ -15,7 +15,8 @@ public class MergeManager : MonoBehaviour
             if (slot.IsEmpty)
             {
                 // Instantiate the draggable tower
-                DraggableImage newTower = Instantiate(towerPrefab, slot.transform);
+
+                DraggableImage newTower = Instantiate(draggableImages[Random.Range(0,draggableImages.Count)], slot.transform);
                 newTower.transform.localPosition = Vector3.zero; // Center it in the slot
                 slot.AssignImage(newTower);
                 newTower.currentSlot = slot;
